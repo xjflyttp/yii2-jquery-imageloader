a12d9faa00b919d65ce0ac91b0c1da7d630e4cc1
xjflyttp <xjflyttp@gmail.com>
2016-09-07 13:31:02 +0800

    init

---
+++ ImageLoaderAsset.php
@@ -0,0 +1,19 @@
+<?php
+namespace xj\jquery\imageloader;
+
+use Yii;
+use yii\web\AssetBundle;
+
+/**
+ * @author xjflyttp <xjflyttp@gmail.cm>
+ */
+class ImageLoaderAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/xj/yii2-jquery-imageloader/assets';
+    public $basePath = '@webroot/assets';
+    public $js = [
+        'jquery.imageloader.js',
+    ];
+    public $depends = ['yii\web\JqueryAsset'];
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # yii2-jquery-imageloader
+```
 https://github.com/beatak/jquery-imageloader/
+```
+
+## composer.json
+```json
+"require": {
+    "xj/yii2-jquery-imageloader": "~1.0.0"
+}
+```
+
+## View
+```
+xj\jquery\imageloader\ImageLoaderAsset::register($this);
+```
